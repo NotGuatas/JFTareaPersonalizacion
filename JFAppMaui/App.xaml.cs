f9216809f3938c634cae81938b7eaebbda9e6ad7
NotGuatas <josephaugustoflores@gmail.com>
2024-11-26 13:17:09 -0500

    Creación de AppMaui

---
+++ JFAppMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JFAppMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JFAppMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JFAppMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
